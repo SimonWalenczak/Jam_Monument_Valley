0738ac24aa0aaffb5e0db99d2ed33b1a1d8afd6b
Simon Walenczak <92367148+SimonWalenczak@users.noreply.github.com>
2024-11-07 10:57:51 +0100

    [DEV] Refacto Input Manager

--- Assets/Scripts/Player/InputManagement.cs
+++ Assets/Scripts/Player/InputManagement.cs
@@ -2,81 +2,97 @@ using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class InputManagement : MonoBehaviour
+namespace Player
 {
-    #region Properties
-
-    [field: SerializeField] public InputsEnum Inputs { get; private set; }
-
-    [SerializeField] private float deadzoneJoystick = 0.3f;
-    [SerializeField] private float deadzoneJoystickTrigger = 0.3f;
-
-    private CharacterMultiplayerManager _characterMultiplayerManager;
-    private GameplayInputs _gameplayInputs;
-    [HideInInspector] public PlayerConfiguration PlayerConfig;
-
-    #endregion
-
-    #region Methods
-
-    private void Awake()
+    /// <summary>
+    /// Manages player input settings and processing, including joystick deadzone adjustments and input gathering.
+    /// </summary>
+    public class InputManagement : MonoBehaviour
     {
-        _gameplayInputs = new GameplayInputs();
-        _gameplayInputs.Enable();
-    }
+        #region Properties
 
-    public void InitializePlayer(PlayerConfiguration pc)
-    {
-        PlayerConfig = pc;
-        PlayerConfig.Input.onActionTriggered += GatherInputs;
-    }
+        [field: SerializeField] public InputsEnum Inputs { get; private set; }
+        [HideInInspector] public PlayerConfiguration PlayerConfig;
 
-    private void GatherInputs(InputAction.CallbackContext context)
-    {
-        InputsEnum inputsEnum = Inputs;
+        [SerializeField] private float _deadzoneJoystick = 0.3f;
+        [SerializeField] private float _deadzoneJoystickTrigger = 0.3f;
 
-        if (context.action.name == _gameplayInputs.Player.MoveNorth.name)
-            inputsEnum.MoveNorth = context.ReadValue<float>() > deadzoneJoystickTrigger;
+        private CharacterMultiplayerManager _characterMultiplayerManager;
+        private GameplayInputs _gameplayInputs;
 
-        if (context.action.name == _gameplayInputs.Player.MoveSouth.name)
-            inputsEnum.MoveSouth = context.ReadValue<float>() > deadzoneJoystickTrigger;
+        #endregion
 
-        if (context.action.name == _gameplayInputs.Player.MoveEast.name)
-            inputsEnum.MoveEast = context.ReadValue<float>() > deadzoneJoystickTrigger;
+        #region Methods
 
-        if (context.action.name == _gameplayInputs.Player.MoveWest.name)
-            inputsEnum.MoveWest = context.ReadValue<float>() > deadzoneJoystickTrigger;
+        private void Awake()
+        {
+            _gameplayInputs = new GameplayInputs();
+            _gameplayInputs.Enable();
+        }
 
-        if (context.action.name == _gameplayInputs.Player.ClickBlock.name)
-            inputsEnum.ClickBlock = context.ReadValue<float>() > deadzoneJoystickTrigger;
+        /// <summary>
+        /// Sets up player-specific configurations and assigns the input action event listener.
+        /// </summary>
+        /// <param name="playerConfig">The player configuration instance to be assigned.</param>
+        public void InitializePlayer(PlayerConfiguration pc)
+        {
+            PlayerConfig = pc;
+            PlayerConfig.Input.onActionTriggered += GatherInputs;
+        }
 
-        if (context.action.name == _gameplayInputs.Player.RotateLeft.name)
-            inputsEnum.RotateLeft = context.ReadValue<float>() > deadzoneJoystickTrigger;
+        /// <summary>
+        /// Processes input actions and updates the <see cref="InputsEnum"/> structure based on defined deadzones.
+        /// </summary>
+        /// <param name="context">The context of the input action event.</param>
+        private void GatherInputs(InputAction.CallbackContext context)
+        {
+            InputsEnum inputsEnum = Inputs;
+
+            if (context.action.name == _gameplayInputs.Player.MoveNorth.name)
+                inputsEnum.MoveNorth = context.ReadValue<float>() > _deadzoneJoystickTrigger;
+
+            if (context.action.name == _gameplayInputs.Player.MoveSouth.name)
+                inputsEnum.MoveSouth = context.ReadValue<float>() > _deadzoneJoystickTrigger;
+
+            if (context.action.name == _gameplayInputs.Player.MoveEast.name)
+                inputsEnum.MoveEast = context.ReadValue<float>() > _deadzoneJoystickTrigger;
+
+            if (context.action.name == _gameplayInputs.Player.MoveWest.name)
+                inputsEnum.MoveWest = context.ReadValue<float>() > _deadzoneJoystickTrigger;
+
+            if (context.action.name == _gameplayInputs.Player.ClickBlock.name)
+                inputsEnum.ClickBlock = context.ReadValue<float>() > _deadzoneJoystickTrigger;
+
+            if (context.action.name == _gameplayInputs.Player.RotateLeft.name)
+                inputsEnum.RotateLeft = context.ReadValue<float>() > _deadzoneJoystickTrigger;
         
-        if (context.action.name == _gameplayInputs.Player.RotateRight.name)
-            inputsEnum.RotateRight = context.ReadValue<float>() > deadzoneJoystickTrigger;
-   
-        
-        inputsEnum.Deadzone = deadzoneJoystick;
+            if (context.action.name == _gameplayInputs.Player.RotateRight.name)
+                inputsEnum.RotateRight = context.ReadValue<float>() > _deadzoneJoystickTrigger;
+            
+            inputsEnum.Deadzone = _deadzoneJoystick;
 
-        Inputs = inputsEnum;
+            Inputs = inputsEnum;
+        }
+
+        #endregion
     }
 
-    #endregion
-}
+    /// <summary>
+    /// Structure representing various player input states and deadzone settings.
+    /// </summary>
+    [Serializable]
+    public struct InputsEnum
+    {
+        public bool MoveNorth;
+        public bool MoveSouth;
+        public bool MoveEast;
+        public bool MoveWest;
 
-[Serializable]
-public struct InputsEnum
-{
-    public bool MoveNorth;
-    public bool MoveSouth;
-    public bool MoveEast;
-    public bool MoveWest;
+        public bool ClickBlock;
 
-    public bool ClickBlock;
+        public bool RotateLeft;
+        public bool RotateRight;
 
-    public bool RotateLeft;
-    public bool RotateRight;
-
-    public float Deadzone;
+        public float Deadzone;
+    }
 }
--- Assets/Scripts/Player/SelectCharacter.cs
+++ Assets/Scripts/Player/SelectCharacter.cs
@@ -2,73 +2,97 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class SelectCharacter : MonoBehaviour
+namespace Player
 {
-    #region Properties
-
-    [field: SerializeField] public Image CharacterImage { get; private set; }
-    [field: SerializeField] public Sprite[] CharacterSprites { get; private set; }
-
-    [SerializeField] private int selectedCharacterIndex = 0;
-
-    [Header("Selection")]
-    [SerializeField] private Image _titlePlayer;
-    [SerializeField] private List<Sprite> _titlePlayerSprites;
-    [SerializeField] private List<GameObject> _meshCharacterPlayers;
-    [SerializeField] private GameObject _actualMesh;
-    [SerializeField] private GameObject _readyPanel;
-    [SerializeField] private GameObject _menuPanel;
-
-    private int _playerIndex;
-
-    #endregion
-
-    #region Methods
-
-    private void Start()
+    /// <summary>
+    /// Manages the selection and customization of the player's character.
+    /// Allows for selecting the next or previous character and setting the character's mesh for gameplay.
+    /// </summary>
+    public class SelectCharacter : MonoBehaviour
     {
-        _actualMesh = _meshCharacterPlayers[0];
+        #region Properties
+
+        [field: SerializeField] public Image CharacterImage { get; private set; }
+        [field: SerializeField] public Sprite[] CharacterSprites { get; private set; }
+
+        [SerializeField] private int _selectedCharacterIndex = 0;
+
+        [Header("Selection")]
+        [SerializeField] private Image _playerTitleImage;
+        [SerializeField] private List<Sprite> _playerTitleSprites;
+        [SerializeField] private List<GameObject> _characterMeshList;
+        [SerializeField] private GameObject _currentCharacterMesh;
+        [SerializeField] private GameObject _readyPanel;
+        [SerializeField] private GameObject _menuPanel;
+
+        private int _playerIndex;
+
+        #endregion
+
+        #region Methods
+
+        private void Start()
+        {
+            _currentCharacterMesh = _characterMeshList[0];
+        }
+
+        /// <summary>
+        /// Sets the player index and updates the player title image based on the index.
+        /// </summary>
+        /// <param name="playerIndex">The index of the player.</param>
+        public void SetPlayerIndex(int pi)
+        {
+            _playerIndex = pi;
+            _playerTitleImage.sprite = _playerTitleSprites[_playerIndex];
+        }
+
+        /// <summary>
+        /// Selects the next character in the list. Loops back to the first character if the end is reached.
+        /// </summary>
+        public void SelectNextCharacter()
+        {
+            if (_selectedCharacterIndex < CharacterSprites.Length - 1)
+                _selectedCharacterIndex = _selectedCharacterIndex + 1;
+            else
+                _selectedCharacterIndex = 0;
+
+            UpdateCharacterSprite();
+        }
+
+        /// <summary>
+        /// Selects the previous character in the list. Loops back to the last character if the beginning is reached.
+        /// </summary>
+        public void SelectPreviousCharacter()
+        {
+            if (_selectedCharacterIndex > 0)
+                _selectedCharacterIndex = _selectedCharacterIndex - 1;
+            else
+                _selectedCharacterIndex = CharacterSprites.Length - 1;
+
+            UpdateCharacterSprite();
+        }
+
+        /// <summary>
+        /// Updates the character sprite and mesh based on the selected character index.
+        /// </summary>
+        void UpdateCharacterSprite()
+        {
+            CharacterImage.sprite = CharacterSprites[_selectedCharacterIndex];
+            _currentCharacterMesh = _characterMeshList[_selectedCharacterIndex];
+        }
+
+        
+        /// <summary>
+        /// Sets the character mesh for the player and moves to the ready panel.
+        /// </summary>
+        public void SetMesh()
+        {
+            PlayerConfigurationManager.Instance.SetPlayerMesh(_playerIndex, _currentCharacterMesh, _selectedCharacterIndex);
+            PlayerConfigurationManager.Instance.ReadyPlayer(_playerIndex);
+            _menuPanel.SetActive(false);
+            _readyPanel.SetActive(true);
+        }
+
+        #endregion
     }
-
-    public void SetPlayerIndex(int pi)
-    {
-        _playerIndex = pi;
-        _titlePlayer.sprite = _titlePlayerSprites[_playerIndex];
-    }
-
-    public void SelectNextCharacter()
-    {
-        if (selectedCharacterIndex < CharacterSprites.Length - 1)
-            selectedCharacterIndex = selectedCharacterIndex + 1;
-        else
-            selectedCharacterIndex = 0;
-
-        UpdateCharacterSprite();
-    }
-
-    public void SelectPreviousCharacter()
-    {
-        if (selectedCharacterIndex > 0)
-            selectedCharacterIndex = selectedCharacterIndex - 1;
-        else
-            selectedCharacterIndex = CharacterSprites.Length - 1;
-
-        UpdateCharacterSprite();
-    }
-
-    void UpdateCharacterSprite()
-    {
-        CharacterImage.sprite = CharacterSprites[selectedCharacterIndex];
-        _actualMesh = _meshCharacterPlayers[selectedCharacterIndex];
-    }
-
-    public void SetMesh()
-    {
-        PlayerConfigurationManager.Instance.SetPlayerMesh(_playerIndex, _actualMesh, selectedCharacterIndex);
-        PlayerConfigurationManager.Instance.ReadyPlayer(_playerIndex);
-        _menuPanel.SetActive(false);
-        _readyPanel.SetActive(true);
-    }
-
-    #endregion
 }
